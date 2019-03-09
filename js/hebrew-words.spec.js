bddd60055f58d4894dc98784879ecc01e82553fd
Daniel Lewis <daniellewis777@gmail.com>
2019-03-09 03:19:18 +0000

    shalom

--- js/hebrew-words.spec.js
+++ js/hebrew-words.spec.js
@@ -16,7 +16,7 @@ describe('hebrew words', () => {
     expect(actual).to.eql(expected)
   })
 
-  it('hebrew word', () => {
+  it('shalom', () => {
     const word = 'שָׁלוֹם'
     const actual = hebrewWords(word)
     const expected = {
