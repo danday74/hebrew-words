8547cdc195ed04b26734f1a097a573bb282005fa
Daniel Lewis <daniellewis777@gmail.com>
2019-03-09 19:46:17 +0000

    hebrew chars are done

--- js/hebrew-chars/index.js
+++ js/hebrew-chars/index.js
@@ -3,10 +3,13 @@ const vowelsSimple = require('./vowels-simple')
 const vowelsComplex = require('./vowels-complex')
 const nonVowels = require('./non-vowels')
 const dagesh = {
+  id: 'O',
   name: 'dagesh',
   char: 'ּ',
-  sounds: ['']
+  sounds: [''],
+  trans: null
 }
+
 const all = [...consonants, ...vowelsSimple, ...nonVowels, dagesh]
 
 module.exports = {
