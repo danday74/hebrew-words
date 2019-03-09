ee699b596f23fa575539a563c6c8ac31a75e54d9
Daniel Lewis <daniellewis777@gmail.com>
2019-03-09 01:24:02 +0000

    redefined all

--- js/hebrew-chars/index.js
+++ js/hebrew-chars/index.js
@@ -2,11 +2,16 @@ const consonants = require('./consonants')
 const vowelsSimple = require('./vowels-simple')
 const vowelsComplex = require('./vowels-complex')
 const nonVowels = require('./non-vowels')
+const dagesh = {
+  name: 'dagesh',
+  value: 'ּ',
+  chars: ['']
+}
 
 module.exports = {
   consonants,
   vowelsSimple,
   vowelsComplex,
   nonVowels,
-  all: [...consonants, ...vowelsSimple, ...vowelsComplex, ...nonVowels]
+  all: [...consonants, ...vowelsSimple, ...nonVowels, dagesh]
 }
