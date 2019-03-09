6fa013b957cd50d34e72dd13716d05cf66820ac2
Daniel Lewis <daniellewis777@gmail.com>
2019-03-09 01:07:55 +0000

    added hebrew chars index

---
+++ js/hebrew-chars/index.js
@@ -0,0 +1,11 @@
+const consonants = require('./consonants')
+const vowelsSimple = require('./vowels-simple')
+const vowelsComplex = require('./vowels-complex')
+const nonVowels = require('./non-vowels')
+
+module.exports = {
+  consonants,
+  vowelsSimple,
+  vowelsComplex,
+  nonVowels
+}
