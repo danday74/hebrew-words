a22d144845340205a2b6e0f698b02a59f7b42589
Daniel Lewis <daniellewis777@gmail.com>
2019-03-16 11:08:40 +0000

    renamed file

--- js/test-layers/consonant/index.js
+++ js/test-layers/consonant/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const tidyHtml = require('../tidy-html')
 
-const shalom = fs.readFileSync(__dirname + '/shalom.html', 'utf8')
+const shalom = fs.readFileSync(__dirname + '/sha-lom.html', 'utf8')
 
 const consonant = {
   shalom: tidyHtml(shalom)
