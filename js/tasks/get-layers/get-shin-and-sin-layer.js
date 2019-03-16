b203b656af31e8c9872d3b289096be81e846d641
Daniel Lewis <daniellewis777@gmail.com>
2019-03-16 16:59:12 +0000

    fixed shin and sin layer

--- js/tasks/get-layers/get-shin-and-sin-layer.js
+++ js/tasks/get-layers/get-shin-and-sin-layer.js
@@ -1,27 +1,28 @@
-const {find, range} = require('lodash')
+const {find} = require('lodash')
 const hebrewChars = require('../../hebrew-chars')
-const consonants = hebrewChars.consonants
+const consonantChars = hebrewChars.consonants.map(consonant => consonant.char)
+const plainConsonantChars = [...consonantChars, 'ש']
+const shin = find(hebrewChars.consonants, {name: 'shin'})
+const sin = find(hebrewChars.consonants, {name: 'sin'})
 
 const getShinAndSinLayer = (obj, $) => {
 
   $('.layer-chunk').addClass('shin-and-sin-layer')
 
-  range(obj.counts.consonants).forEach(i => {
+  let i = 0
+  let prevChar
 
-    const shinAndSinChunk = $(`.letter-${i} .shin-and-sin-chunk`)
+  const chars = obj.word.split('')
+  chars.forEach(char => {
+    const isConsonant = plainConsonantChars.includes(char)
+    if (isConsonant) i++
     const contentChunk = $(`.letter-${i} .content-chunk`)
-    if (contentChunk.text() === 'ש') {
-      if (shinAndSinChunk.hasClass('shin-dot')) {
-        const consonant = find(consonants, {name: 'shin'})
-        contentChunk.text(consonant.char)
-        /* istanbul ignore else */
-      } else if (shinAndSinChunk.hasClass('sin-dot')) {
-        const consonant = find(consonants, {name: 'sin'})
-        contentChunk.text(consonant.char)
-      } else {
-        throw Error('Failed to add shin or sin dot')
-      }
+    if (prevChar === 'ש') {
+      if (char.charCodeAt(0) === 1473) contentChunk.text(shin.char) // shin dot
+      else if (char.charCodeAt(0) === 1474) contentChunk.text(sin.char) // sin dot
+      else throw Error('shin dot or sin dot must follow shin or sin')
     }
+    prevChar = char
   })
 
   // obj.layers.shinAndSin = $('body').html()
